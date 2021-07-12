c48160bece02054d431eb1cd015d164bdd568021
littleicecream <78819905+littleicecream@users.noreply.github.com>
2021-07-12 21:39:29 +0600

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,22 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1782134960:AAFIAEXf0eDtG5atyUNgZ4a-qbQgoqqd_XA"
+  APP_ID = 2469678
+  API_HASH = "d692515312741702751ad5525dbd91c3"
+  AUTH_CHANNEL = [-521884514]
+  RCLONE_CONFIG = """""
+  DESTINATION_FOLDER = "Gdrive bot"
+  GLEECH_COMMAND = "gleech@lyujiaojunbot"
+  INDEX_LINK = "https://drive.dragondrive1233.workers.dev/0:/Anime"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@lyujiaojunbot"
+  CANCEL_COMMAND_G = "cancel@lyujiaojunbot"
+  GET_SIZE_G = "getsize@lyujiaojunbot"
+  STATUS_COMMAND = "status@lyujiaojunbot"
+  PYTDL_COMMAND_G = "pytdl@lyujiaojunbot"
+  LOG_COMMAND = "log@lyujiaojunbot"
+  CLONE_COMMAND_G = "clone@lyujiaojunbot"
+  UPLOAD_COMMAND = "upload@lyujiaojunbot"
+  RENEWME_COMMAND = "rename@lyujiaojunbot"
+  
+  
